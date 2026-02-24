1f5186f980623bdd4f402165d4b1cb298c934a21
adhu <adhavdhanavel@gmail.com>
2026-02-24 12:02:33 +0530

    removed authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Blind Builder backend running!");
